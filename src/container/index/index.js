bf02f451f2129e86b3193843336ee89f95299a97
Tala261012 <tala261012@gmail.com>
2023-08-11 00:35:15 +0300

    js-layout is done

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,100 @@
-import '../../script/test'
+// мой вариант  ---------------------------------------------------
+// const h1 = (title) => {
+//   const h1 = document.createElement('h1')
+//   h1.innerHTML = title
+//   h1.className = 'title'
 
-console.log('container')
+//   return h1
+// }
+
+// let page = document.querySelector('.page')
+
+// page.append(h1('Мiй блог'))
+//-------------------------------------------------------------------
+
+import {
+  createElement,
+  createHeader,
+} from '../../script/layout'
+
+const page = document.querySelector('.page')
+
+const header = createHeader()
+
+page.append(header)
+
+const title = createElement('h1', 'title', 'Мiй блог')
+
+page.append(title)
+
+//-----------------------------------------------------------------------
+
+const POST_LIST = [
+  {
+    category: [
+      { text: 'Важливо', id: 1 },
+      { text: 'Нова', id: 2 },
+    ],
+    info: 'До біса планувальник, наймаємо дизайнера і готуємося до презентації, як Джобс',
+    date: '25.01',
+    viewed: false,
+  },
+  {
+    category: [{ text: 'Важливо', id: 1 }],
+    info: 'Ми хотіли щоб у цьому чаті було близько 150 людей щоб зробити якісний пак самопрезентацій.',
+    date: '15.01',
+    viewed: true,
+  },
+]
+
+const createPost = () => {
+  const postList = createElement('main', 'post__list')
+
+  POST_LIST.forEach((postData) => {
+    const post = createElement(
+      'div',
+      postData.viewed
+        ? 'post button post--viewed'
+        : 'post button',
+    )
+
+    const postHeader = createElement('div', 'post__header')
+
+    const categoryList = createElement(
+      'div',
+      'post__category-list',
+    )
+
+    postData.category.forEach((category) => {
+      const categorySpan = createElement(
+        'span',
+        `post__category post__category--${category.id}`,
+        category.text,
+      )
+      categoryList.append(categorySpan)
+    })
+
+    const dateSpan = createElement(
+      'span',
+      'post__date',
+      postData.date,
+    )
+
+    postHeader.append(categoryList, dateSpan)
+
+    const infoParagraph = createElement(
+      'p',
+      'post__info',
+      postData.info,
+    )
+
+    post.append(postHeader, infoParagraph)
+    postList.append(post)
+  })
+
+  return postList
+}
+
+const post = createPost()
+
+page.append(post)
